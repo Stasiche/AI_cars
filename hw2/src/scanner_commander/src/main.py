d7bdb6fdc41d21618218eb2486a92802ac85d813
Stanislav Lebedev <lebedevsf239@gmail.com>
2021-10-26 12:52:40 +0300

    tmp

--- hw2/src/scanner_commander/src/main.py
+++ hw2/src/scanner_commander/src/main.py
@@ -14,7 +14,7 @@ class LaserScanContainer:
         self.angle_increment = None
         self.ranges = None
         self.cart = None
-
+        self.filter_mask = None
 
     def set_params(self, msg):
         self.angle_min = float(msg.angle_min)
@@ -27,13 +27,32 @@ class LaserScanContainer:
     def convert_to_cartesian(self):
         max_r = max(self.ranges)
         self.cart = np.zeros((len(self.ranges), 2))
-        for i, r in enumerate(self.ranges):
-            phi = i*self.angle_increment + self.angle_min
 
-            self.cart[i, 0] = r * cos(phi)
-            self.cart[i, 1] = r * sin(phi)
+        mask = self.filter_mask if self.filter_mask is not None else [True]*len(self.ranges)
 
-            
+        for i, (r, m) in enumerate(zip(self.ranges, mask)):
+            if m:
+                phi = i*self.angle_increment + self.angle_min
+
+                self.cart[i, 0] = r * cos(phi)
+                self.cart[i, 1] = r * sin(phi)
+
+    def create_filter_mask(self, treshold=0.1):
+        self.filter_mask = [True]
+        for cur_id in range(1, len(self.ranges)-1):
+            prev, cur, nxt = self.ranges[cur_id-1:cur_id+2]
+            mean = (nxt + prev)/2
+            prev_dif = cur - prev
+            nxt_dif  = nxt - cur
+            ratio = abs(prev_dif - nxt_dif)/mean
+
+            self.filter_mask.append(int(ratio < treshold))
+        self.filter_mask.append(True)
+
+    def save_img(self, name):
+        plt.scatter(self.cart[:, 0], self.cart[:, 1])
+        plt.savefig(f'{name}.png')
+        
 
 
 def callback(msg):
@@ -41,9 +60,14 @@ def callback(msg):
     if not scan.inited:
         scan.set_params(msg)
         scan.convert_to_cartesian()
-        plt.scatter(scan.cart[:, 0], scan.cart[:, 1])
-        plt.savefig('tmp.png')
+        scan.save_img('tmp')
+
+        scan.create_filter_mask()
+        scan.convert_to_cartesian()
+        scan.save_img('tmp_filtered')
         
+        
+
 
 rospy.init_node('log')
 scan = LaserScanContainer()
