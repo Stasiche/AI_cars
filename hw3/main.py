5571089fb5bae8db83c1a7b4bd413e241dd2ae69
Stanislav Lebedev <lebedevsf239@gmail.com>
2021-12-08 16:12:07 +0300

    mf

--- hw3/main.py
+++ hw3/main.py
@@ -3,22 +3,10 @@ import gym
 
 import pyglet
 from pyglet.window import key
+from recoder import VideoRecorder
 
 import numpy as np
 from gym_duckietown.envs import DuckietownEnv
-
-env = DuckietownEnv(
-    seed=1,
-    map_name='straight_road',
-    draw_curve=False,
-    draw_bbox=False,
-    domain_rand=False,
-    frame_skip=1,
-    distortion=False,
-    camera_rand=False,
-    dynamics_rand=False,
-)
-
 
 def straight(steps: int, k: int):
     for _ in range(steps):
@@ -32,13 +20,36 @@ def turn(steps: int, k: int):
         env.render()
 
 
+def change_side(k):
+    turn(100, k * 1)
+    straight(125, 1)
+    turn(100, k * -1)
+
+
+def go_around():
+    change_side(1)
+    straight(50, 1)
+    change_side(-1)
+
+
+env = DuckietownEnv(
+    seed=1,
+    map_name='straight_road',
+    draw_curve=False,
+    draw_bbox=False,
+    domain_rand=False,
+    frame_skip=1,
+    distortion=False,
+    camera_rand=False,
+    dynamics_rand=False,
+)
+
+env = VideoRecorder(env, './results')
+
+
 env.reset()
 img, _, _, _ = env.step(np.array([0, 0]))
-
 straight(20, 1)
-turn(100, 1)
-straight(125, 1)
-turn(100, -1)
-straight(500, 1)
-
+go_around()
+straight(20, 1)
 env.close()
